7b9a5ea405506cfda45a81e622f46c37f2b82a71
Pavel Kulesh <kulesh.p.v.b.2.m@gmail.com>
2020-03-04 21:47:40 +0300

    add morze-decoder solution

--- src/index.js
+++ src/index.js
@@ -1,46 +1,64 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
+function splittingIntoSubstrings(string, count) { // charArray = expr.match(/.{1,10}/g);
+  const result = [];
+  for (let i = 0; i < string.length; i += count) {
+    result.push(string.substring(i, i + count));
+  }
+  return result;
+}
+
 function decode(expr) {
-    // write your solution here
+  const charArray = splittingIntoSubstrings(expr, 10);
+
+  return charArray.map((char) => {
+    if (char === '**********') return ' ';
+    const morzeLetter = splittingIntoSubstrings(char, 2).map((number) => {
+      if (number === '00') return '';
+      if (number === '10') return '.';
+      if (number === '11') return '-';
+    }).join('');
+    return MORSE_TABLE[morzeLetter];
+  }).join('');
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
